2f51e5866b2e23325a9bf92b211d34a7793235c2
hzhuyuanyuan <hzhuyuanyuan@corp.netease.com>
2018-04-24 14:59:16 +0800

    添加helloworld接口

---
+++ src/main/java/com/user/signin/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.user.signin.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Author yyhu3
+ * @Date 2018-04-24 14:53
+ */
+
+@RestController
+public class LoginController {
+    @GetMapping("/HelloWorld")
+    public String HelloWorld()
+    {
+        return "HelloWorld";
+    }
+}
